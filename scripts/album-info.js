8bc556ae797add95bc8142cc4e0856b0eb8beaf5
Daniela <dguerrera87@gmail.com>
2019-11-23 09:46:19 +0200

    Working through assignment for checkpoint 18

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -4,5 +4,3 @@ $('img#album-cover-art').attr('src', album.albumArtUrl);
 $('#release-info').text(album.releaseInfo);
 $('.artist').text(album.artist);
  }
- //testing//
- //more testing
--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -26,22 +26,26 @@
 
   //Previous button
   $('button#previous').on('click', function() {
-    if (player.playState !== 'playing') { return; }
+      if (player.playState !== 'playing') { return; }
 
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex - 1;
-    if (nextSongIndex >= album.songs.length) { return; }
+      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+      const previousSongIndex = currentSongIndex - 1;
+      if (previousSongIndex < 0) { return; }
 
-    const nextSong = album.songs[nextSongIndex];
-    helper.playPauseAndUpdate(nextSong);
-  });
+      const previousSong = album.songs[previousSongIndex];
+      helper.playPauseAndUpdate(previousSong);
+    });
 
-  setInterval( () => {
-    if (player.playState !== 'playing') { return; }
-    const currentTime = player.getTime();
-    const duration = player.getDuration();
-    const percent = (currentTime / duration) * 100;
-    $('#time-control .current-time').text( currentTime );
-    $('#time-control input').val(percent);
-  }, 1000);
+    $('#time-control input').on('input', function (event) {
+      player.skipTo(event.target.value);
+    });
+
+    setInterval( () => {
+      if (player.playState !== 'playing') { return; }
+      const currentTime = player.getTime();
+      const duration = player.getDuration();
+      const percent = (currentTime / duration) * 100;
+      $('#time-control .current-time').text( currentTime );
+      $('#time-control input').val(percent);
+    }, 1000);
 }
